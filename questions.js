1c6d8c240c04cdd2c0a960c1a407353145a5d178
Jonas.J <jan.jonas.johansson@gmail.com>
2019-08-25 19:50:00 +0200

    initial commit ⭐️

---
+++ questions.js
@@ -0,0 +1,238 @@
+exports.Questions = [
+  /*
+    Points: 100
+    Level: Easy
+  */
+  {
+    question: "What is/was your mothers name?",
+    tags: ["family", "parent"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "What is/was your fathers name?",
+    tags: ["family", "parent"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "How tall are you?",
+    tags: ["personal", "fact"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "What is your last name?",
+    tags: ["personal", "fact"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "What is the name of the city you currently live in?",
+    tags: ["personal", "geolocation"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "What is your eye color?",
+    tags: ["personal", "fact"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "Which is your dominant hand?",
+    tags: ["personal", "fact"],
+    points: 100,
+    level: 'easy',
+  },
+  {
+    question: "Which is your dominant foot?",
+    tags: ["personal", "fact"],
+    points: 100,
+    level: 'easy',
+  },
+  /*
+    Points: 200
+    Level: Easy
+  */
+  {
+    question: "What is your shoe size?",
+    tags: ["personal", "fact"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "How old are you?",
+    tags: ["personal", "fact"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "Which year were you born?",
+    tags: ["personal", "fact"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "What is your current occupation?",
+    tags: ["personal", "work"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "How many siblings do you have?",
+    tags: ["personal", "family", "siblings"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "Name 5 apps on your phone?",
+    tags: ["personal", "phone"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "Which phone do you currently use?",
+    tags: ["personal", "phone", "belongings"],
+    points: 200,
+    level: 'easy',
+  },
+  {
+    question: "Do you prefer coffee, tea or something else?",
+    tags: ["personal", "taste"],
+    points: 200,
+    level: 'easy',
+  },
+  /*
+    Points: 300
+    Level: Easy
+  */
+  {
+    question: "When is your birthday?",
+    tags: ["personal", "fact"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "Which sibling are you (oldest, youngest, etc)?",
+    tags: ["personal", "family", "sibling", "fact"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "What is your favorite sports team?",
+    tags: ["personal", "sport"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "Do you prefer sun-, snow- or city-vacation?",
+    tags: ["personal", "preferences", "geolocation"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "Where do you consider yourself \"from\"?",
+    tags: ["personal", "geolocation"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "Which is your favorite pizza?",
+    tags: ["personal", "taste", "favorites"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "What is your favorite number?",
+    tags: ["personal", "favorites"],
+    points: 300,
+    level: 'easy',
+  },
+  {
+    question: "What is your favorite book?",
+    tags: ["personal", "favorites"],
+    points: 300,
+    level: 'easy',
+  },
+  /*
+    Points: 500
+    Level: Easy
+  */
+  {
+    question: "What is your middle name?",
+    tags: ["personal", "fact"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "How many languages do you speak?",
+    tags: ["personal", "skills"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "Which is you favorite music band?",
+    tags: ["personal", "favorites", "music"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "Where would you like to move?",
+    tags: ["personal", "future", "geolocation"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "What education do you have?",
+    tags: ["personal", "education"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "Which sport(s) are you practising?",
+    tags: ["personal", "sport"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "Which season is your favorite (autumn, winter, spring, summer)?",
+    tags: ["personal", "favorites"],
+    points: 500,
+    level: 'easy',
+  },
+  {
+    question: "What is your partner's lastname?",
+    tags: ["personal", "relationships"],
+    points: 500,
+    level: 'easy',
+  },
+  /*
+    Points: 800
+    Level: Easy
+  */
+  {
+    question: "Which pets have you had?",
+    tags: ["personal", "pets"],
+    points: 800,
+    level: 'easy',
+  },
+  {
+    question: "What was the name of your highschool (swedish gymnasium)?",
+    tags: ["personal", "education"],
+    points: 800,
+    level: 'easy',
+  },
+  {
+    question: "Which type of drink do you prefer (beer, soda, lemonade, whine, whiskey, etc)?",
+    tags: ["personal", "taste"],
+    points: 800,
+    level: 'easy',
+  },
+  {
+    question: "Which brand of mascara up do you prefer?",
+    tags: ["personal", "make up", "preferences"],
+    points: 800,
+    level: 'easy',
+  }
+]
---
+++ utils.js
@@ -0,0 +1,33 @@
+const EMOJIS = ['🐹', '🐶', '🐱', '🦊', '🐻', '🐯', '🦁', '🐸', '🐵', '🐥'];
+const COLORS = ['#f368e0', '#ff9f43', '#ee5253', '#ee5253', '#10ac84', '#5f27cd', '#795548', '#8BC34A'];
+
+
+const getItemsNotInRoom = (state, itemType) => {
+  return state.players
+      .map((playerId) => (state[playerId] || {})[itemType])
+      .filter((item) => typeof item !== 'undefined');
+}
+
+const pickUniqueEmoiji = (state) => {
+  const filteredEmojis = EMOJIS.filter((emoji) => !getItemsNotInRoom(state, 'emoji').includes(emoji))
+  return filteredEmojis[Math.floor(Math.random() * filteredEmojis.length)];
+}
+
+const pickUniqueColor = (state) => {
+  const filteredColors = COLORS.filter((color) => !getItemsNotInRoom(state, 'color').includes(color))
+  return filteredColors[Math.floor(Math.random() * filteredColors.length)];
+}
+
+const setIntervalX = (fn, Xms, times) => {
+  if(!times) return
+  setTimeout(() => {
+    fn() 
+    setIntervalX(fn, Xms, times-1)
+  }, Xms)
+}
+
+module.exports = {
+  pickUniqueEmoiji: pickUniqueEmoiji,
+  pickUniqueColor: pickUniqueColor,
+  setIntervalX: setIntervalX,
+}
